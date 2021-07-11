f0a7b2d95cdcb263e4db2e4f67ca169d2d518f51
PrivacyStatement <86305809+PrivacyStatement@users.noreply.github.com>
2021-07-11 14:41:28 +0200

    recreate project

---
+++ main.py
@@ -0,0 +1,15 @@
+from flask import Flask , render_template, url_for
+
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template('Start.html')
+
+@app.route("/Load_Game")
+def dddd():
+    return render_template('Load_Game.html')
+
+
+app.run(host= "0.0.0.0", port=5000)
