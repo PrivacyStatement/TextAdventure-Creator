37b254e5780a79cf4e50802be66bbe1ad2df772c
PrivacyStatement <86305809+PrivacyStatement@users.noreply.github.com>
2021-07-18 22:56:54 +0200

    mutch

--- main.py
+++ main.py
@@ -15,4 +15,12 @@ def Load():
 def Game():
     return render_template('Game.html')
 
+@app.route("/Load_Creator")
+def Load2():
+    return render_template('Load_Creator.html')
+
+@app.route("/Creator")
+def Creator():
+    return render_template('Creator.html')
+
 app.run(host= "0.0.0.0", port=5000)
