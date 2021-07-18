a5830238e9775d7de368c2b397e6e70c421b6a79
PrivacyStatement <86305809+PrivacyStatement@users.noreply.github.com>
2021-07-18 16:07:54 +0200

    Game.html is ready

--- main.py
+++ main.py
@@ -8,7 +8,11 @@ def index():
     return render_template('Start.html')
 
 @app.route("/Load_Game")
-def dddd():
+def Load():
     return render_template('Load_Game.html')
+
+@app.route("/Game")
+def Game():
+    return render_template('Game.html')
 
 app.run(host= "0.0.0.0", port=5000)
